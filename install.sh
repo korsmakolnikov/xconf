7f1c8437ea61296916c61e9f24232337d398ca57
Federico Caprari <korsmakolnikov@gmail.com>
2020-11-06 09:49:58 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+<<<<<<< HEAD
+ln -s /home/blacksheep/conf/.vimrc /home/blacksheep/.vimrc
+=======
+ln -s /home/blacksheep/xconf/.vimrc /home/blacksheep/.vimrc
+ln -s /home/blacksheep/xconf/bash_conf/.bashlib /home/blacksheep/.bashlib
+ln -s /home/blacksheep/xconf/bash_conf/.bash_profile /home/blacksheep/.bash_profile
+ln -s /home/blacksheep/xconf/bash_conf/.bash_prompt /home/blacksheep/.bash_prompt
+ln -s /home/blacksheep/xconf/bash_conf/.bash_docker /home/blacksheep/.bash_docker
+ln -s /home/blacksheep/xconf/bash_conf/.bashlib /home/blacksheep/.bashlib
+ln -s /home/blacksheep/xconf/bash_conf/.bash_aliases /home/blacksheep/.bash_aliases
+>>>>>>> 597c31f... fixup! install script
