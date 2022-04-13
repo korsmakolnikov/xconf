9ea5701502c88f372864060aa2c88fae74feca62
korsmakolnikov <korsmakolnikov@gmail.com>
2022-04-13 10:27:23 +0200

    Install k9s/aws cli

--- asdf.sh
+++ asdf.sh
@@ -11,4 +11,8 @@ asdf global rust 1.58.1
 asdf global nodejs 16.14.2 
 sudo pacman --noconfirm -S rust-analyzer
 sudo pacman --noconfirm -S python-pip
+sudo pacman --noconfirm -S k9s
 go get -v github.com/dcoker/biscuit
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
