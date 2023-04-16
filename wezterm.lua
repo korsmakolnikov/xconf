10d266dacdf31d8c0aa6e7bdaf4c5f9242d1e90d
Federico Caprari <korsmakolnikov@gmail.com>
2023-04-16 12:24:23 +0200

    Set fullscreen on startup

--- wezterm.lua
+++ wezterm.lua
@@ -61,4 +61,10 @@ config.keys = {
   }
 }
 
+wezterm.on('gui-startup', function(cmd)
+  local _, _, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
+end)
+
+
 return config
