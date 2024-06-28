a54c4ce989126af296d52392e9c852b40b1b7f69
korsmakolnikov <korsmakolnikov@gmail.com>
2024-06-28 20:22:48 +0200

    Improve markdown configuration

--- nvim/lua/markdown.lua
+++ nvim/lua/markdown.lua
@@ -1,4 +1,9 @@
+local m = {}
+m.md_setup = function()
+  vim.cmd(":setlocal spell spelllang=en_us,it")
+  vim.cmd(":set tw=80")
+end
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*.md,*.vpc,*.txt",
-  command = ":setlocal spell spelllang=en_us,it",
+  callback = m.md_setup
 })
